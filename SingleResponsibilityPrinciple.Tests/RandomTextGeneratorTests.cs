12fd76a36716cf5d6eabd67b39c82ae26d7f8b66
xalaren <imanov.03.valera@gmail.com>
2025-03-12 17:03:44 +0500

    Added tests for RandomTextGenerator

---
+++ SingleResponsibilityPrinciple.Tests/RandomTextGeneratorTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using SingleResponsibilityPrinciple.TextGen;
+
+namespace SingleResponsibilityPrinciple.Tests;
+
+[TestFixture]
+public class RandomTextGeneratorTests
+{
+    private RandomTextGenerator randomTextGen;
+    
+    [SetUp]
+    public void Setup()
+    {
+        randomTextGen = new RandomTextGenerator();
+    }
+
+    [TestCase(0)]
+    [TestCase(20)]
+    public void CheckCorrespondingTextLength_GenerateTest(int expectedTextLength)
+    {
+        var generatedText = randomTextGen.Generate(expectedTextLength);
+        Assert.That(generatedText.Length, Is.EqualTo(expectedTextLength));
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(-10)]
+    public void CheckStringEmpty_GenerateTest(int textLength)
+    {
+        var generatedText = randomTextGen.Generate(textLength);
+        Assert.IsEmpty(generatedText);
+    }
+    
+    [TestCase(1)]
+    [TestCase(10)]
+    [TestCase(1)]
+    public void CheckStringIsNotNullAndNotEmpty_GenerateTest(int textLength)
+    {
+        var generatedText = randomTextGen.Generate(textLength);
+        Assert.IsNotNull(generatedText);
+        Assert.IsNotEmpty(generatedText);
+    }
+}
