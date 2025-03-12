980526dd96a09064d29baba56ccc5db402e43f36
xalaren <imanov.03.valera@gmail.com>
2025-03-12 13:50:36 +0500

    Add Test project. Rollback projects version to .NET 8

---
+++ SingleResponsibilityPrinciple.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System.Security.Cryptography;
+
+namespace SingleResponsibilityPrinciple.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
