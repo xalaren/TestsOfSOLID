819001b45f30b222d2c2a4ed223714792a0c4d7a
xalaren <imanov.03.valera@gmail.com>
2025-03-14 16:05:59 +0500

    Add Triangle.cs

---
+++ OpenClosedPrinciple/Triangle.cs
@@ -0,0 +1,63 @@
+﻿namespace OpenClosedPrinciple;
+
+public class Triangle : Shape
+{
+    private double firstSideLength;
+    private double secondSideLength;
+    private double thirdSideLength;
+
+    public double FirstSideLength
+    {
+        get => firstSideLength;
+        set
+        {
+            if (value < 0) return;
+            firstSideLength = value;
+        }
+    }
+    
+    public double SecondSideLength
+    {
+        get => secondSideLength;
+        set
+        {
+            if (value < 0) return;
+            secondSideLength = value;
+        }
+    }
+    
+    public double ThirdSideLength
+    {
+        get => thirdSideLength;
+        set
+        {
+            if (value < 0) return;
+            thirdSideLength = value;
+        }
+    }
+    
+    public override double CalculateArea()
+    {
+        if (!IsTriangleExists()) return 0;
+        
+        var halfPerimeter = CalculatePerimeter() / 2.0;
+        return Math.Round
+        (
+            Math.Sqrt(halfPerimeter * (halfPerimeter - firstSideLength) * (halfPerimeter - secondSideLength) *
+                         (halfPerimeter - thirdSideLength)), 2
+        );
+    }
+
+    private double CalculatePerimeter()
+    {
+        return firstSideLength + secondSideLength + thirdSideLength;
+    }
+
+    private bool IsTriangleExists()
+    {
+        return
+            firstSideLength + secondSideLength >= thirdSideLength &&
+            secondSideLength + thirdSideLength >= firstSideLength &&
+            firstSideLength + thirdSideLength >= secondSideLength;
+    }
+}
